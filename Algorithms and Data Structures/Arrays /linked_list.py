ab2f116a3d6de62e989703c30eddbb71bf5c65e0
Darryl Johnson <dandycodes@gmail.com>
2022-12-30 21:04:46 -0500

    found out how to create and add list nodes

--- Algorithms and Data Structures/Arrays /linked_list.py
+++ Algorithms and Data Structures/Arrays /linked_list.py
@@ -1,3 +1,5 @@
+# nodes are the basis for linked lists
+
 class Node: 
     data = None
     next_node = None
@@ -6,3 +8,21 @@ class Node:
         self.data = data
     def __repr__(self):
         return "<Node data: %s>" % self.data
+    
+    
+class LinkedList: 
+    def __init__(self):
+        self.head = None
+        
+    def is_empty(self):
+        return self.head == None
+    
+    def size(self):
+        current = self.head
+        count = 0
+        
+        while current:
+            count += 1
+            current = current.next_node
+            
+        return count 
