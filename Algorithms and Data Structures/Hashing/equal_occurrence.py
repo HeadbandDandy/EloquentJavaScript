23b73e8b531a2d78290fd1f0ab0d5dc6875d98d3
Darryl Johnson <dandycodes@gmail.com>
2023-03-27 12:18:54 -0400

    one line solution added

--- Algorithms and Data Structures/Hashing/equal_occurrence.py
+++ Algorithms and Data Structures/Hashing/equal_occurrence.py
@@ -1,7 +1,7 @@
 # sets ignore duplicates so we can put all frequency values into a set and check if the length is 1
 
 
-from collections import defaultdict
+from collections import defaultdict, Counter
 
 class Solution:
     def areOccurrencesEqual(self, s: str) -> bool:
@@ -12,3 +12,10 @@ class Solution:
         
         frequencies = counts.values()
         return len(set(frequencies)) == 1 
+    
+    
+    
+
+class SecondSolution:
+    def areOccurrencesEqual(self, s: str) -> bool:
+        return len(set(Counter(s).values())) == 1
